914a0b1c06a5d45d9d5adf919de51d00904c1c35
Lucas Leite <lucas.leitedosreis@gmail.com>
2022-04-20 21:04:05 -0300

    fix: mudando arquivo com endereços das imagens para o arquivo main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 // Enmu listando os grupos associados com a imagem
-import WEATHER_MAIN_CODES_IMAGES from '/codeImages.js';
+let WEATHER_MAIN_CODES_IMAGES = {
+  Thunderstorm: 'tempestade.png',
+  Drizzle: 'chuva.png',
+  Rain: 'chuva.png',
+  Snow: 'neve.png',
+  Clear: 'sol.png',
+  Clouds: 'nublado.png',
+  Mist: 'mist.png',
+  Haze: 'haze.png',
+  Dust: 'dust.png',
+  Smoke: 'smoke.png',
+  Fog: 'fog.png',
+  Sand: 'sand.png',
+  Ash: 'ash.png',
+  Squall: 'squall.png',
+  Tornado: 'tornado.png',
+};
 
 //weather forecast
 const api = {
